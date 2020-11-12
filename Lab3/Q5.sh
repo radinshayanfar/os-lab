fe5b23104e4b47eb242c5f1779fb97f5228d85a8
Radin Shayanfar <radin.shayanfar@gmail.com>
2020-11-12 04:07:31 +0330

    Lab3 Q5 added.

---
+++ Lab3/Q5.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+read -r -d '' txt1 << EOM
+1
+22
+333
+4444
+55555
+EOM
+
+read -r -d '' txt2 << EOM
+​     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * *
+* * * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+EOM
+
+read -r -d '' txt3 << EOM
+|
+---
+|  |
+   ---
+|  |  |
+      ---
+|  |  |  |
+         ---
+|  |  |  |  |
+            ---
+EOM
+
+texts=("$txt1" "$txt2" "$txt3")
+
+read -p "Enter a number between 1-3: " num
+((num--))
+echo "${texts[num]}"
